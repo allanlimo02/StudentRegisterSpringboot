4c6d3618c74923070e1de5bc238b28bca053de52
allanlimo02 <allanlimo02@gmail.com>
2022-01-27 16:59:07 +0300

    Initial commit

---
+++ src/test/java/com/mekaki/allaney/AllaneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mekaki.allaney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllaneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
